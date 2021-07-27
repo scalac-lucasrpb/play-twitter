282a98345dcf308a972926519f837976689c7bad
lucasrpb <lucasrpb@gmail.com>
2021-07-27 20:58:50 -0300

    Started to implement OAuth2 auth

--- app/actions/OAuth2Action.scala
+++ app/actions/OAuth2Action.scala
@@ -3,14 +3,19 @@ package actions
 import play.api.Logging
 import play.api.mvc._
 
-import javax.inject.Inject
+import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
+@Singleton
 class OAuth2Action @Inject() (parser: BodyParsers.Default)(implicit ec: ExecutionContext)
   extends ActionBuilderImpl(parser)
     with Logging {
   override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = {
-    logger.info(s"${request.headers.get("Authorization")}")
-    block(request)
+    request.headers.get("Authorization") match {
+      case None => Future.successful(Results.Unauthorized("Authorization token not found!"))
+      case Some(auth) =>
+        logger.debug(s"Authorization token found: ${auth}! Here we would check JWT user and secret!")
+        block(request)
+    }
   }
 }
